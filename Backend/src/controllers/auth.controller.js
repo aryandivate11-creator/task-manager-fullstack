23642e4cc8e021e54976cde249a82b464c1a026b
aryandivate11-creator <aryandivate11@gmail.com>
2025-12-24 18:55:36 +0530

    backend login and signup validation is ready

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,13 +1,109 @@
-export const signup = (req,res) =>{
-     res.json({
-        message:"Signup Route hit",
-        data: req.body,
-     })
-}
+import bcrypt from "bcrypt";
+import pool from "../config/db.js";
 
-export const login = (req,res) =>{
-     res.json({
-        message:"Login route hit",
-        data: req.body,
-     })
-}
+export const signup = async (req,res) =>{
+    const {email , password , username, phone} = req.body;
+    console.log(req.body);
+
+    if (!email || !password || !username ){
+      return res.status(400).json({
+         error:"Email, password and username are required !"
+      });
+    }
+    
+    try{
+      const [existinguser] = await pool.query(
+         "SELECT id FROM users WHERE email = ?",
+         [email]
+      );
+
+      if(existinguser.length > 0){
+        return res.status(400).json({
+          error:"User already exists !"
+        });
+      }
+
+      const hashedpassword = await bcrypt.hash(password,10);
+      console.log("Hashed password:", hashedpassword);
+
+      await pool.query(
+         `INSERT INTO users (username, email, phone, password)
+         VALUES (?, ?, ?, ?)`,
+         [username , email , phone , hashedpassword]
+      );
+
+       res.status(201).json({
+         message:"User Signed up sucessfully !!!"
+      });
+
+    } catch(error){
+      console.error(error);
+      res.status(500).json({
+         error:"Internal Server Error"
+      });
+    }
+
+   //  if (password.length < 6){
+   //    return res.status(400).json({
+   //       error:"Password must be atleast 6 characters long !"
+   //    });
+   //  }
+   
+   //  if(!email.includes("@")){
+   //    return res.status(400).json({
+   //       error:"Please enter a valid email address !"
+   //    })
+   //  }
+
+//     res.status(201).json({
+//       message:"User registered sucessfully !!!",
+//       user :{email},
+//     })
+};
+
+export const login = async (req,res) =>{
+      const {email, password} = req.body;
+
+      if(!email || !password){
+         return res.status(400).json({
+            error:"Email and password are required"
+         });
+      }
+
+     try {
+      const [users] = await pool.query(
+         "SELECT * FROM users WHERE email = ?",
+         [email]
+      );
+      
+      if(users.length === 0){
+         return res.status(400).json({
+            error:"Invalid email or password !"
+         })
+      };
+
+      const user = users[0];
+
+      const isPasswordValid = await bcrypt.compare(password , user.password);
+
+      if(!isPasswordValid){
+         return res.status(400).json({
+            error:"Invalid email or password !"
+         })
+      };
+
+      res.status(200).json({
+         message:"Login Successful",
+         user:{
+            id: user.id,
+            username: user.username,
+            email: user.email,
+         },
+      });
+     } catch (error) {
+      console.error(error);
+      res.status(500).json({
+         error:"Internal Server Error"
+      });
+     }
+};
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,3 +1,4 @@
+import pool from "./config/db.js";
 import dotenv from 'dotenv';
 import app from './app.js';
 
@@ -11,6 +12,19 @@ app.get('/',(req,res) =>{
     res.send('<h1>Hello TechStalwarts</h1');
 })
 
+const testDBConnection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("MySQL connected successfully ✅");
+    connection.release();
+  } catch (error) {
+    console.error("MySQL connection failed ❌", error);
+  }
+};
+
+testDBConnection();
+
 app.listen(port,() =>{
     console.log(`Server running on http://localhost:${port}`);
 })
+
